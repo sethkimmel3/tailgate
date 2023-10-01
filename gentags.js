454a49aca39037341678de2a920944d3cdf4ac2f
Seth Kimmel <seth.kimmel3@gmail.com>
2023-09-30 22:15:02 -0700

    move to fastapi routes

--- gentags.js
+++ gentags.js
@@ -1,56 +1,72 @@
-function initGentags(key=''){
-    // get all elements with data attribute data-gentag
-    for (const element of document.querySelectorAll('[data-gentag]')) {
-        if (element.tagName == "P") {
-            processGenTag(element, "text", key);
+class GenTags {
+    constructor(api_key, hydrate_on_load=true) {
+        this.api_key = api_key;
+        this.base_url = "https://sethkimmel3--gentags-api-app.modal.run";
+        if (hydrate_on_load) {
+            this.hydrate_gentags();
         }
-        else if (element.tagName == "IMG") {
-            processGenTag(element, "image", key);
+        // this.test_gentags_api();
+    }        
+
+    hydrate_gentags(elems=null) {
+        if (elems == null) {
+            elems = document.querySelectorAll('[data-gentag]');
+        }
+        else {
+            elems = document.querySelectorAll(elems);
+        }
+        for (const element of elems) {
+            if (element.tagName == "P") {
+                this.hydrate_gentag(element, "text");
+            }
+            else if (element.tagName == "IMG") {
+                this.hydrate_gentag(element, "image");
+            }
         }
     }
-    // test_gentags_api(key);
-}
 
-async function test_gentags_api(api_key=''){
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        console.log(this.responseText);
+    test_gentags_api(){
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            console.log(this.responseText);
+        }
+        xhttp.open("POST", this.base_url, true);
+        xhttp.setRequestHeader("Content-type", "application/json");
+        xhttp.setRequestHeader('x-api-key', this.api_key);
+        xhttp.send(JSON.stringify({"query": "test!"}));
     }
-    url = "https://sethkimmel3--gentags-api-f.modal.run"
-    // add body to request
-    xhttp.open("POST", url, true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.setRequestHeader('x-api-key', api_key);
-    xhttp.send(JSON.stringify({"query": "test!"}));
-}
 
-function processGenTag(elem, type, api_key=''){
-    prompt = elem.attributes['data-prompt'].value;
-    // send prompt to gentags api
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        let response = this.responseText;
-        // strip quotes from beginning and end of response
-        if (response[0] == '"') {
-            response = response.substring(1, response.length - 1);
+    hydrate_gentag(elem, type){
+        prompt = elem.attributes['data-prompt'].value;
+        // send prompt to gentags api
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            let response = this.responseText;
+            // strip quotes from beginning and end of response
+            if (response[0] == '"') {
+                response = response.substring(1, response.length - 1);
+            }
+            if (response[response.length - 1] == '"') {
+                response = response.substring(0, response.length - 2);
+            }
+            if (type == "text") {
+                elem.innerHTML = response;
+            }
+            else if (type == "image") {
+                elem.src = response;
+            }  
         }
-        if (response[response.length - 1] == '"') {
-            response = response.substring(0, response.length - 2);
-        }
+        let url = "";
         if (type == "text") {
-            elem.innerHTML = response;
+            url = this.base_url + "/generate-text";
         }
         else if (type == "image") {
-            elem.src = response;
-        }  
+            url = this.base_url + "/generate-image";
+        }
+        xhttp.open("POST", url, true);
+        xhttp.setRequestHeader("Content-type", "application/json");
+        xhttp.setRequestHeader('x-api-key', this.api_key);
+        xhttp.send(JSON.stringify({"prompt": prompt}));
     }
-    if (type == "text") {
-        url = "https://sethkimmel3--gentags-api-generate-text.modal.run?prompt=" + encodeURIComponent(prompt);
-    }
-    else if (type == "image") {
-        url = "https://sethkimmel3--gentags-api-generate-image.modal.run?prompt=" + encodeURIComponent(prompt);
-    }
-    xhttp.open("GET", url, true);
-    xhttp.setRequestHeader('x-api-key', api_key);
-    xhttp.send();
 }
+
--- server/server.py
+++ server/server.py
@@ -1,8 +1,10 @@
 import modal
 import openai
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from urllib.parse import urlparse
+import os
 
 image = (
     modal.Image.debian_slim()
@@ -11,6 +13,14 @@ image = (
 
 stub = modal.Stub("gentags-api", image=image)
 web_app = FastAPI()
+
+web_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 domain_whitelist = {
     'example-public-key': ['localhost', 'sethkim.me'],
@@ -25,18 +35,22 @@ def check_key_origin(request):
         return False
     return True
 
-@stub.function()
-@modal.web_endpoint(method="POST")
-def f(request: Request):
-    if not check_key_origin(request):
+@web_app.post("/")
+def f(req: Request):
+    if not check_key_origin(req):
         return {"error": "Invalid origin"}
 
     return {"Hello": "World"}
 
-@stub.function(secret=modal.Secret.from_name("openai-secret"))
-@modal.web_endpoint()
-def generate_text(request: Request, prompt, max_tokens=250, temperature=0.5):
-    if not check_key_origin(request):
+class GenerateTextRequest(BaseModel):
+    prompt: str
+    max_tokens: int = 250
+    temperature: float = 0.5
+
+@web_app.post("/generate-text")
+def generate_text(request: GenerateTextRequest, req: Request):
+    prompt, max_tokens, temperature = request.prompt, request.max_tokens, request.temperature
+    if not check_key_origin(req):
         return {"error": "Invalid origin"}
 
     messages = [
@@ -52,10 +66,13 @@ def generate_text(request: Request, prompt, max_tokens=250, temperature=0.5):
     
     return response['choices'][0].message['content'].replace('\n', '<br>')
 
-@stub.function(secret=modal.Secret.from_name("openai-secret"))
-@modal.web_endpoint()
-def generate_image(request: Request, prompt, max_tokens=250, temperature=0.5):
-    if not check_key_origin(request):
+class GenerateImageRequest(BaseModel):
+    prompt: str
+
+@web_app.post("/generate-image")
+def generate_image(request: GenerateImageRequest, req: Request):
+    prompt = request.prompt
+    if not check_key_origin(req):
         return {"error": "Invalid origin"}
 
     response = openai.Image.create(
@@ -66,6 +83,11 @@ def generate_image(request: Request, prompt, max_tokens=250, temperature=0.5):
 
     image_url = response['data'][0]['url']
     return image_url
+
+@stub.function(secret=modal.Secret.from_name("openai-secret"))
+@modal.asgi_app()
+def app():
+    return web_app
 
 if __name__ == "__main__":
     stub.serve()
